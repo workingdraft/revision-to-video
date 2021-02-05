75a14902dd5af0ab43ad885a81eef2059c69e4fd
Peter Kröner <peter@peterkroener.de>
2021-02-05 14:26:37 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const fs = require("fs").promises;
+const rimraf = require("rimraf");
+const { execSync } = require("child_process");
+const fetch = require("node-fetch");
+const parser = new (require("rss-parser"))();
+const { createCanvas, loadImage } = require("canvas");
+
+async function getLatestFeedItem() {
+  console.log("Reading feed...");
+  const feed = await parser.parseURL("https://workingdraft.de/feed/");
+  console.log(`Proceeding with feed item "${feed.items[0].title}"...`);
+  return { title: feed.items[0].title, url: feed.items[0].enclosure.url };
+}
+
+async function downloadAudio({ title, url }) {
+  console.log(`Downloading audio...`);
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Got status ${response.status} while fetching audio`);
+  }
+  const audioFile = `tmp/${title}.mp3`;
+  await fs.writeFile(audioFile, await response.buffer());
+  return audioFile;
+}
+
+function parseTitle(title) {
+  const match = /^Revision ([0-9]+)(?::(.+))?/.exec(title);
+  if (!match) {
+    console.warn(`WARNING: Unable to parse title "${title}", check if generated thumbnail is ok`);
+    return { nr: -1, text: null };
+  }
+  return { nr: match[1], text: match[2] };
+}
+
+async function generateThumbnail(title) {
+  console.log(`Rendering thumbnail...`);
+  const { nr, text } = parseTitle(title);
+  const tagText = (nr > -1) ? `#${nr}` : `#spezial`;
+  const subText = (nr === -1 || !text) ? title : text;
+  const image = await loadImage("img/video.png");
+  const maxTextWidth = image.width - 2 * 250;
+  const canvas = createCanvas(image.width, image.height);
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(image, 0, 0);
+  ctx.font = "bold 400px Source Sans Pro"
+  ctx.textAlign = "right";
+  ctx.fillStyle = "#910c69";
+  ctx.strokeStyle = "#FFF";
+  ctx.lineWidth = 80;
+  ctx.strokeText(tagText, canvas.width - 250, canvas.height - 300);
+  ctx.fillText(tagText, canvas.width - 250, canvas.height - 300);
+  ctx.font = "bold 200px Source Sans Pro";
+  ctx.lineWidth = 40;
+  ctx.strokeText(subText, canvas.width - 250, canvas.height - 100, maxTextWidth);
+  ctx.fillText(subText, canvas.width - 250, canvas.height - 100, maxTextWidth);
+  const thumbnailFile = `tmp/${title}.png`;
+  await fs.writeFile(thumbnailFile, canvas.toBuffer("image/png"));
+  return thumbnailFile;
+}
+
+function encode(title, audioFile, thumbnailFile) {
+  const videoFile = `out/${title}.mp4`;
+  console.log(`Encoding "${videoFile}". This will take some time...`);
+  execSync(`ffmpeg -loop 1 -i "${thumbnailFile}" -i "${audioFile}" -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest "${videoFile}" > log.txt`);
+}
+
+(async function() {
+  rimraf.sync("out/*")
+  rimraf.sync("tmp/*")
+  const { url, title } = await getLatestFeedItem();
+  const [ audioFile, thumbnailFile ] = await Promise.all([
+    downloadAudio({ url, title }),
+    generateThumbnail(title),
+  ]);
+  const videoFile = encode(title, audioFile, thumbnailFile);
+  console.log(`Done! Now upload "${videoFile}" to https://studio.youtube.com/channel/UCTJTfsq21-sC6maSTzifiPQ/videos/upload?d=ud`);
+})();
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Revision to video
+
+How to:
+
+1. Install [Source Sans Pro](https://fonts.google.com/specimen/Source+Sans+Pro) and [ffmpeg](https://ffmpeg.org/)
+2. `npm install`
+3. `node index.js` to videoify the latest revision
+4. Wait for ffmpeg to finish
+5. Upload manually
